a70842b5875832ee16727e0ecb0c95791b5b88bc
Hadawale15 <shubhamhadawale15@gmail.com>
2023-08-28 13:32:13 +0530

    second commit after changing latex view

--- app/src/main/java/com/example/cil_mathview/MainActivity.java
+++ app/src/main/java/com/example/cil_mathview/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cil_mathview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import ru.noties.jlatexmath.JLatexMathDrawable;
 import ru.noties.jlatexmath.JLatexMathView;
@@ -10,16 +12,22 @@ import ru.noties.jlatexmath.JLatexMathView;
 public class MainActivity extends AppCompatActivity {
 
     JLatexMathView latexMathView;
+  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
         latexMathView=findViewById(R.id.j_latex_math_view);
 
-         JLatexMathDrawable drawable = JLatexMathDrawable.builder("\\begin{array}{l}\na_{1} x+b_{1} y+c_{1}=0 \\\\\na_{2} x+b_{2} y+c_{2}=0 \\\\\na_{1}^{2}+b_{1}^{2} \\neq 0 \\& a_{2}^{2}+b_{2}^{2} \\neq 0 \\\\\nx=\\frac{-b \\pm \\sqrt{b^{2}+4 a c}}{2 a} \\\\\np^{m} \\times p^{n}=p^{m+n} \\\\\n\\left\\{p^{m}\\right\\} /\\left\\{p^{n}\\right\\}=p^{m-n} \\\\\np^{-m}=\\frac{1}{p^{m}} \\quad n^{\\text {th }} \\text { term }=a+(n-1) d \\\\\nS_{n}=\\frac{n}{2}[2 a+(n-1) d] \\\\\n\\sin \\theta=\\frac{\\text { Side opposite to angle } \\theta}{\\text { Hypotenuse }}=\\frac{P}{H} \\\\\n\\operatorname{cosec} \\theta=\\frac{1}{\\sin \\theta} \\quad \\sin \\left(90^{\\circ}-\\theta\\right)=\\cos \\theta \\\\\n\\sin { }^{2} \\theta+\\cos ^{2} \\theta=1 \\\\\n\\sec ^{2} \\theta=1+\\tan ^{2} \\theta \\text { for } 0^{\\circ} \\leqslant \\theta<90^{\\circ} \\\\\n\\operatorname{cosec}^{2} \\theta=1+\\cot ^{2} \\theta \\text { for } 0^{\\circ} \\leqslant 0<90^{\\circ}\n\\end{array}"
-
-                        )
+                 JLatexMathDrawable drawable = JLatexMathDrawable.builder("\\text {  \\begin{array}{l}\n\\left(\\frac{a_{1}}{a_{2}}\\right),\\left(\\frac{b_{1}}{b_{2}}\\right),\\left(\\begin{array}{c}\nc_{1} \\\\\nc_{2}\n\\end{array}\\right) \\mid x=\\frac{D_{x}}{D}, y=\\frac{D_{y}}{D} \\\\\n2) \\frac{4}{x-y}+\\frac{1}{x+y}=3 ; \\frac{2}{x-y}-\\frac{3}{x+y}=5 \\\\\n\\rightarrow \\frac{4}{x-y}+\\frac{1}{x+y}=3 ; \\frac{2}{x-y}-\\frac{3}{x+y}=5 \\\\\n4\\left(\\frac{1}{x-y}\\right)+1\\left(\\frac{1}{x+y}\\right)=3 \\ldots \\ldots \\text { (I) } \\\\\n2\\left(\\frac{1}{x-y}\\right)-3\\left(\\frac{1}{x+y}\\right)=5 \\ldots \\text { (II) } \\\\\n4 a+b=3 \\ldots \\text { (III) }\n\\end{array}\\\\\n Ans Acid contains \\( (H+) \\) hydrogen ions and vased contains \\\\\n \\( (10 \\mathrm{H}) \\) hydroxide ions. Salt and water are furmed by\\\\\ncombination of acid and vase.\\\\\n\\[\\\\\n\\begin{array}{l}\\\\\n\\text { Acid + Base } \\rightarrow \\text { Salt + water } \\\\\n\\mathrm{HCl}+\\mathrm{NaOH} \\rightarrow \\mathrm{NaCl}+\\mathrm{H}_{2} \\mathrm{O} .\\\\\n\\end{array}\\\\\n\\]\\\\\nThis chemical reaction is neutrillization}"
+                         )
                 .textSize(70)
                 .padding(8)
                 .background(0xFFffffff)
